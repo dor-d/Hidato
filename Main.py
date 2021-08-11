091890d608ec68995c6ffa0ab320378fd448d0b4
Peleg <pelz94@gmail.com>
2021-08-11 15:45:07 +0300

    Prettify main some more

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import time
 from HidatoGenerator import *
 from HidatoCSP import *
 from SolverCSP import *
+import sys
 
 
 def is_consistent(hidato):
@@ -30,9 +31,7 @@ def timeit(func):
         print(f'Running {func.__name__} took ' + '{0:.4g}'.format(end) + ' seconds.')
     return timed_func
 
-@timeit
 def solve_hidato(width, height):
-
     print(f'Solving a {width} x {height} Hidato...\n')
 
     gen = HidatoGenerator()
@@ -40,19 +39,23 @@ def solve_hidato(width, height):
     print("Before solve:")
     hidato.display()
 
-    solver = SolverCSP()
-    solver.solve(hidato)
+    sys.stdout.flush()
+
+    _solve(hidato)
     print("\nAfter solve:")
     hidato.display()
 
     correct = is_consistent(hidato)
     print(f"Solution is {'correct' if correct else 'incorrect'}.")
 
-
+@timeit
+def _solve(hidato):
+    solver = SolverCSP()
+    solver.solve(hidato)
 
 def main():
     random.seed(0)
-    width = height = 5
+    width = height = 10
     solve_hidato(width, height)
 
 
