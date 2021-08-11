4bd3f3da6dc9504e75460790e559ccd2f89d390e
Peleg <pelz94@gmail.com>
2021-08-11 15:28:13 +0300

    add timeit to main

--- Main.py
+++ Main.py
@@ -1,7 +1,24 @@
+import time
+
 from HidatoGenerator import *
 from HidatoCSP import *
 from SolverCSP import *
 
+DIM = 15
+
+
+def timeit(func):
+    def timed_func():
+        start = time.time()
+        func()
+        end = time.time() - start
+        print(f'Running {func.__name__} took ' + '{0:.4g}'.format(end) + ' seconds.')
+    return timed_func
+
+
+# def solve_hidato
+
+@timeit
 def main():
     random.seed(0)
 
