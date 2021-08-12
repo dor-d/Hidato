e321edaa24d0ada7aefa4eb7ef94c5f1d0119a6a
Peleg <pelz94@gmail.com>
2021-08-12 18:24:50 +0300

    Prettify timing prints

--- Main.py
+++ Main.py
@@ -5,17 +5,24 @@ from HidatoCSP import *
 from SolverCSP import *
 import sys
 
-
-DIM = 14
+DIM = 13
 
 
 def timeit(func):
     def timed_func(*args, **kwargs):
         start = time.time()
         func(*args, *kwargs)
-        end = time.time() - start
-        print(f'Running {func.__name__} took ' + '{0:.4g}'.format(end) + ' seconds.')
+        total = time.time() - start
+        if total < 60:
+            print(f'Running {func.__name__} took ' + '{0:.4g}'.format(total) + ' seconds.')
+        else:
+            mins = total // 60
+            secs = total % 60
+            print(
+                f'Running {func.__name__} took {mins} minute{"s" if mins > 1 else ""} and {"{0:.4g}".format(secs)} seconds.')
+
     return timed_func
+
 
 @timeit
 def solve_hidato(width, height):
@@ -34,15 +41,17 @@ def solve_hidato(width, height):
 
     print(f"Solution is {'correct' if hidato.is_correct() else 'incorrect'}.")
 
+
 @timeit
 def _solve(hidato):
     solver = SolverCSP()
     solver.solve(hidato)
 
+
 def main():
     random.seed(0)
-    # width = height = DIM
-    solve_hidato(6, 5)
+    width = height = DIM
+    solve_hidato(width, height)
 
 
 if __name__ == '__main__':
